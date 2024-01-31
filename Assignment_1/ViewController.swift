45238e65b24905e960cfe8ac3bd53613227269e0
Reinamimi <benedictreina@gmail.com>
2024-01-31 11:37:47 -0500

    Initial Commit

---
+++ Assignment_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment_1
+//
+//  Created by mac on 31/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
